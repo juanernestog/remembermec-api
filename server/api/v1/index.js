7b02b85ecbaad5c9d4b6bfaaede39296add81df7
JuanE <juanernestogranados@gmail.com>
2022-09-01 18:16:58 -0500

    configured routers of mahines and maintenances

--- server/api/v1/index.js
+++ server/api/v1/index.js
@@ -2,8 +2,13 @@ const express = require('express');
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
+
 const users = require('./users/routes');
+const machines = require('./machines/routes');
+const maintenances = require('./maintenances/routes');
 
 router.use('/users', users);
+router.use('/machines', machines);
+router.use('/maintenances', maintenances);
 
 module.exports = router;
